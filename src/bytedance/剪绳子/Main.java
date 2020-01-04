358221bfc2dca774240d13a22bd6296d6c89954c
zzzzer <zzzzer91@gmail.com>
2020-01-04 18:03:24 +0800

    添加字节跳动题目

---
+++ src/bytedance/剪绳子/Main.java
@@ -0,0 +1,47 @@
+package bytedance.剪绳子;
+
+import java.util.Scanner;
+
+/**
+ * https://www.acwing.com/problem/content/682/
+ *
+ * 使用二分法
+ */
+class Main {
+    public static boolean check(int[] ropes, int m, double len) {
+        int count = 0;
+        for (int rope : ropes) {
+            count += rope / len;
+            if (count >= m) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), m = in.nextInt();
+        int[] ropes = new int[n];
+        for (int i = 0; i < n; i++) {
+            ropes[i] = in.nextInt();
+        }
+        int maxLen = ropes[0];
+        for (int i = 1; i < n; i++) {
+            if (maxLen < ropes[i]) {
+                maxLen = ropes[i];
+            }
+        }
+        double l = 0, r = maxLen;
+        while (r - l > 1e-4) {
+            double mid = (l + r) / 2;
+            if (check(ropes, m, mid)) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+
+        }
+        System.out.printf("%.2f\n", l);
+    }
+}
