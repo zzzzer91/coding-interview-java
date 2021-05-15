6c7979fc04759128ae9f2af3f5d6addfe5dab2ec
zzzzer <zzzzer91@gmail.com>
2021-05-15 20:51:06 +0800

    feat: update

--- src/剑指/p03_数组中重复的数字/Solution.java
+++ src/剑指/p03_数组中重复的数字/Solution.java
@@ -1,7 +1,24 @@
 package 剑指.p03_数组中重复的数字;
 
+/**
+ * https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+ */
 class Solution {
-    public int duplicateInArray(int[] nums) {
+    public int findRepeatNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (i != nums[i]) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+                int tmp = nums[i];
+                nums[i] = nums[tmp];
+                nums[tmp] = tmp;
+            }
+        }
+        return 0;
+    }
+
+    public int findRepeatNumber2(int[] nums) {
         for (int num : nums) {
             if (num < 0 || num >= nums.length) {
                 return -1;
--- src/剑指/p04_二维数组中的查找/Solution.java
+++ src/剑指/p04_二维数组中的查找/Solution.java
@@ -1,14 +1,17 @@
 package 剑指.p04_二维数组中的查找;
 
+/**
+ * https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+ */
 class Solution {
-    public boolean searchArray(int[][] array, int target) {
-        int m = array.length;
-        if (m == 0) {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0) {
             return false;
         }
-        int n = array[0].length;
-        for (int i = 0, j = n-1; i < n && j >= 0;) {
-            int val = array[i][j];
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n-1;
+        while (i < m && j >= 0) {
+            int val = matrix[i][j];
             if (val < target) {
                 i++;
             } else if (val > target) {
